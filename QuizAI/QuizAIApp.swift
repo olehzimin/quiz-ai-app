eecf7da402862780020cefc660c4a60668d61f41
Oleh Zimin <ziminoleh@gmail.com>
2025-06-12 20:55:34 +0200

    Initial Commit

---
+++ QuizAI/QuizAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAIApp.swift
+//  QuizAI
+//
+//  Created by Oleh Zimin on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
